01295de64ab48d2c26c5ad017edce3082bf00966
unknown <anish1204>
2024-01-17 23:09:08 +0530

    Video added to Background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import video1 from '../assets/video.mp4'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
-        
+      <section className='Home'>
+        <div className="Overlay">
+          <video src={video1} autoPlay muted loop type='video/mp4'></video>
+            <div className='Home-Container'>
+              <span className='Upper'>
+                  Our Packages
+              </span>
+
+            </div>
+        </div>
+
+      </section>
     </div>
   )
 }
