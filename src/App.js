3418ba9bff355c3ab4bf070c18ccd05a019b63ee
unknown <anish1204>
2024-01-22 12:59:22 +0530

    Places to visit added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-
+import MVP from './components/MVP'
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Home/>
+      <MVP/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,10 +8,23 @@ import { FaSnapchat } from "react-icons/fa6";
 import { IoOptionsOutline } from "react-icons/io5";
 import { TbApps } from "react-icons/tb";
 import { IoLocationOutline } from "react-icons/io5";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+
+import pic1 from '../assets/pic1.jpg';
+import pic2 from '../assets/pic2.jpg';
+import pic3 from '../assets/pic3.jpg';
+import pic4 from '../assets/pic4.jpg';
+import pic5 from '../assets/pic5.jpg';
+import pic6 from '../assets/pic6.jpg';
+import pic7 from '../assets/pic7.jpg';
+import pic8 from '../assets/pic8.jpg';
+import pic9 from '../assets/pic9.jpg';
+
 const Home = () => {
   return (
-    <div>
-      <section className='Home'>
+    <>
+    
+    <section className='Home'>
         <div className="Overlay">
           <video src={video1} autoPlay muted loop type='video/mp4'></video>
           <div className='Home-Container'>
@@ -41,23 +54,275 @@ const Home = () => {
             </div>
             <button className='Filter-button'>< FaFilter className='Filter-Icon'/>MORE FILTERS</button>
             </form>
-            <div className='Social-Handles'>
-              {/* <div className='left-icons'> */}
+            {/* <div className='Social-Handles'>
+            
               <a className='Handle-icons' href='https://fonts.google.com/specimen/Roboto'>{<FaFacebook className='Handle-icons1' /> }</a>
                 <a className='Handle-icons' href='https://fonts.google.com/specimen/Roboto'>{<FaInstagram className='Handle-icons1'/>}</a>
                 <a className='Handle-icons' href='https://fonts.google.com/specimen/Roboto'>{<FaSnapchat className='Handle-icons1' />}</a>
                 <a className='Handle-icons' href='https://fonts.google.com/specimen/Roboto'>{<IoOptionsOutline className='Handle-icons1' />}</a>
                 <a className='Handle-icons' href='https://fonts.google.com/specimen/Roboto'>{<TbApps className='Handle-icons1' />}</a>
-              {/* </div> */}
+              
                 
             
-            </div>
+            </div> */}
             
           </div>
         </div>
 
       </section>
-    </div>
+      {/* Now Starting with the Most visted places sections */}
+      <br>
+      </br>
+      <div className='MostVp' >
+      <h2>Most Visted Places</h2>
+      </div>
+      <div className='CardSection'>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic1} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic2} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic3} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+      </div>
+
+            <br/><br/><br/><br/>
+      <div className='CardSection'>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic4} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic5} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic6} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+      </div>
+      <br/><br/><br/><br/>
+      <div className='CardSection'>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic7} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic8} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+            <div className='Card'>
+              <div className='card-img'>
+                  <img src={pic9} />
+              </div>
+              <div className='card-loc'>
+                    <h3>Bora Bora <br/> <IoLocationOutline/>      New Zealand
+                    </h3>
+                    <p></p>
+              </div><br/>
+              <div className='card-cat'>
+                  <div className='category'>
+                
+                      <h5>CULTURAL<br></br>RELAX</h5>
+                  </div>
+                  <div className='card-amt'>
+                      <h2>$500</h2>
+                  </div>
+              </div>
+              <br></br>
+              <div className='card-descp'>
+                  <p>Bora Bora is a small South Pacific island known for its stunning turquoise lagoon and overwater bungalows, making it a popular luxury travel destination.</p>
+              </div>
+              <div className='card-button'>
+                  <button>BOOK NOW</button>
+              </div>
+            </div>
+      </div>
+    
+      
+      
+    </>
   )
 }
 
